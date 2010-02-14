b0ee363722e7f74056860159e83d457ecbc16fc7
mollifier <mollifier@gmail.com>
2010-02-14 22:40:57 +0900

    実際に tweet するように変更

--- tweetShortcut.js
+++ tweetShortcut.js
@@ -1,8 +1,13 @@
 jetpack.future.import("pageMods");
 
 var callback = function(doc){
-  console.log("load callback");
-  jetpack.notifications.show("Hello Jetpack!");
+  var textBox = $("#status", doc);
+  textBox.keydown(function(e) {
+    if (e.keyCode === 13 && textBox.value !== "") {
+      // 13 : Enter key code
+      $("#status_update_form", doc).submit();
+    }
+  });
 };
 
 var options = {};
